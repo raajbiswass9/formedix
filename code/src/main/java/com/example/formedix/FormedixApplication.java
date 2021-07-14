058ec326020967bb5711722012a534959556f95f
raj039 <raj.biswas936@gmail.com>
2021-07-14 15:21:08 +0100

    Created spring boot structure

---
+++ code/src/main/java/com/example/formedix/FormedixApplication.java
@@ -0,0 +1,13 @@
+package com.example.formedix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormedixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormedixApplication.class, args);
+	}
+
+}
---
+++ code/src/test/java/com/example/formedix/FormedixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formedix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormedixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
