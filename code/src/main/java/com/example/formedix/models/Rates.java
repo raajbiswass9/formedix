567c2056c695e884ba1dc8c0b3d4c959daf96670
raj039 <raj.biswas936@gmail.com>
2021-07-14 16:31:42 +0100

    Created rates model

---
+++ code/src/main/java/com/example/formedix/models/Rates.java
@@ -0,0 +1,69 @@
+package com.example.formedix.models;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name="rates", indexes = @Index(name="idx_date_currency", columnList = "date_id, currency_id"))
+public class Rates {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Integer id;
+
+    @Column(name = "date_id", updatable = false, nullable = false)
+    @Type(type="date")
+    private Date date_id;
+
+    @Column(name = "currency_id", updatable = false, nullable = false)
+    private Integer currency_id;
+
+    @Column(name = "exchange_rate", updatable = false, nullable = true)
+    private float exchange_rate;
+
+    public Rates(Integer id, Date date_id, Integer currency_id, float exchange_rate) {
+        this.id = id;
+        this.date_id = date_id;
+        this.currency_id = currency_id;
+        this.exchange_rate = exchange_rate;
+    }
+
+    public Rates() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate_id() {
+        return date_id;
+    }
+
+    public void setDate_id(Date date_id) {
+        this.date_id = date_id;
+    }
+
+    public Integer getCurrency_id() {
+        return currency_id;
+    }
+
+    public void setCurrency_id(Integer currency_id) {
+        this.currency_id = currency_id;
+    }
+
+    public float getExchange_rate() {
+        return exchange_rate;
+    }
+
+    public void setExchange_rate(float exchange_rate) {
+        this.exchange_rate = exchange_rate;
+    }
+}
