2ddbf111fc64bb86624ab663f4e9f1fec9d69439
raj039 <raj.biswas936@gmail.com>
2021-07-14 16:49:11 +0100

    Created reference_rate method

---
+++ code/src/main/java/com/example/formedix/controllers/MainController.java
@@ -0,0 +1,37 @@
+package com.example.formedix.controllers;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class MainController {
+    Map<String, Object> response = new HashMap<>();
+
+    /**
+     * Get Reference Rate for all currencies on a given date
+     * @param date
+     * @return reference rate
+     */
+    @RequestMapping(value = "/reference_rate", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> getReferenceRate(@RequestParam(value = "date", required = true) String date){
+        response.clear();
+        try {
+            response.put("status:","success");
+            response.put("data:", "");
+        }catch(Exception e) {
+            response.put("status:","fail");
+            response.put("message:",e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
