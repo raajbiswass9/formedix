e75fec05deee2ba05e7dbfcbb30aac4d1297f97c
raj039 <raj.biswas936@gmail.com>
2021-07-18 12:14:54 +0100

    Added Class uml diagram and Rquest flow diagram

--- code/src/main/java/com/example/formedix/repositories/DateRepositoryBase.java
+++ code/src/main/java/com/example/formedix/repositories/DateRepositoryBase.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Date;
 
 public interface DateRepositoryBase  extends JpaRepository<Dates, Integer> {
-
     Dates findAllByDates(Date date);
-
 }
